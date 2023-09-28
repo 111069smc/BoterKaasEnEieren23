5cf8e52710d3ccf16b5275629fd88ad14ed41f1d
MarinaLeenhouts <10851367-MarinaLeenhouts@users.noreply.replit.com>
2023-09-28 17:55:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from bke import EvaluationAgent, start, can_win
+
+
+class MijnSpeler(EvaluationAgent):
+
+  def evaluate(self, board, my_symbol, opponent_symbol):
+    getal = 1
+    if can_win(board, opponent_symbol):
+      getal = getal - 1000
+    return getal
+
+
+mijn_speler = MijnSpeler()
+start(player_o=mijn_speler)
