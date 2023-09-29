881df30fa1fdc6d4f0b9fd4e105b879b1f13be2b
111069smc <95483938+111069smc@users.noreply.github.com>
2023-09-29 07:51:18 +0000

    github geconnect (de hiervoor uitgevoerde commits waren nog niet gekoppeld aan mijn github account)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 from bke import MLAgent, is_winner, opponent, RandomAgent, train_and_plot
-
+ 
 class MyAgent(MLAgent):
     def evaluate(self, board):
         if is_winner(board, self.symbol):
@@ -10,20 +10,15 @@ class MyAgent(MLAgent):
         else:
             reward = 0
         return reward
-
+ 
 random.seed(1)
-
-my_agent = MyAgent() 
+ 
+my_agent = MyAgent()
 random_agent = RandomAgent()
-
-#hier wordt de functie aangeroepen die traint en tegelijk de voortgang plot
+ 
 train_and_plot(
     agent=my_agent,
     validation_agent=random_agent,
     iterations=50,
     trainings=100,
     validations=1000)
-
-#iteration betekent herhaling
-#Het argument trainings geeft per herhaling aan hoeveel er getraind wordt.
-#validations komt neer op het aantal validatiespellen dat per herhaling gespeeld wordt
