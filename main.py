f7f4f0fd9df1e5223ed3545fa47a4d230d516990
111069smc <95483938+111069smc@users.noreply.github.com>
2023-09-29 10:54:31 +0000

    final commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 
-from bke import MLAgent, is_winner, opponent, load, validate, RandomAgent, plot_validation, train, train_and_plot, save, start
+from bke import MLAgent, is_winner, opponent, load, validate, RandomAgent, plot_validation, train, train_and_plot, save
 
 
 class MyAgent(MLAgent):
@@ -20,6 +20,7 @@ random.seed(1)
 my_agent = MyAgent(alpha=0.1, epsilon=0.5)
 random_agent = RandomAgent()
 
+#hier wordt de agent getraint
 train_and_plot(
     agent=my_agent,
     validation_agent=random_agent,
@@ -27,21 +28,14 @@ train_and_plot(
     trainings=100,
     validations=1000)
 
-#agent wordt opnieuw aageroepen
-my_agent = load('MyAgent_3000')
+
 my_agent.learning = False
 
 validation_agent = RandomAgent()
 
+#laat de getrainde agent tegen een rnaadom agent spelen
 validation_result = validate(agent_x=my_agent, agent_o=validation_agent, iterations=100)
  
 plot_validation(validation_result)
 
-#train functie aanroepen
-#train(my_agent, 3000)
-
 save(my_agent, 'MyAgent_3000')
-
-start(player_x=my_agent)
-
-
