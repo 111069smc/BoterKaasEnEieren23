cfd61825d759e7baf904c1d1039a9ba877ca9258
111069smc <95483938+111069smc@users.noreply.github.com>
2023-09-29 10:41:31 +0000

    grafie, chart en spel werken, de agent wint 9 van de 10 keer

--- main.py
+++ main.py
@@ -15,17 +15,19 @@ class MyAgent(MLAgent):
 
 random.seed(1)
 
-my_agent = MyAgent(alpha=0.8, epsilon=0.2)
+#alpha is de leerfactor van de agent. Deze bepaalt hoe snel de agent nieuwe kennis adopteert. Hoe hoger dit getal, hoe sneller de agent geneigd zal zijn om oude kennis te vervangen door nieuwe kennis.
+#epsilon is de verkenningsfactor van de agent. Deze bepaalt hoe vaak de agent nieuwe dingen probeert. Hoe hoger dit getal, hoe vaker de agent een willekeurige actie probeert in plaats van de best bekende zet.
+my_agent = MyAgent(alpha=0.1, epsilon=0.5)
 random_agent = RandomAgent()
 
 train_and_plot(
     agent=my_agent,
     validation_agent=random_agent,
-    iterations=20,
+    iterations=100,
     trainings=100,
     validations=1000)
 
-#agent worst opnieuw aageroepen
+#agent wordt opnieuw aageroepen
 my_agent = load('MyAgent_3000')
 my_agent.learning = False
 
@@ -36,7 +38,7 @@ validation_result = validate(agent_x=my_agent, agent_o=validation_agent, iterati
 plot_validation(validation_result)
 
 #train functie aanroepen
-train(my_agent, 3000)
+#train(my_agent, 3000)
 
 save(my_agent, 'MyAgent_3000')
 
