39477c215156de8da73504c7b41e7291ec2a7d4d
111069smc <95483938+111069smc@users.noreply.github.com>
2023-09-29 10:02:01 +0000

    code af, chart en grafiek worden weergegeven en spel doet het goed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from bke import MLAgent, is_winner, opponent, load, train, save, start
+import random
+
+from bke import MLAgent, is_winner, opponent, load, validate, RandomAgent, plot_validation, train, train_and_plot, save, start
 
 
 class MyAgent(MLAgent):
@@ -10,13 +12,28 @@ class MyAgent(MLAgent):
         else:
             reward = 0
         return reward
-    
 
-my_agent = MyAgent()
+random.seed(1)
+
+my_agent = MyAgent(alpha=0.8, epsilon=0.2)
+random_agent = RandomAgent()
+
+train_and_plot(
+    agent=my_agent,
+    validation_agent=random_agent,
+    iterations=20,
+    trainings=100,
+    validations=1000)
 
 #agent worst opnieuw aageroepen
 my_agent = load('MyAgent_3000')
 my_agent.learning = False
+
+validation_agent = RandomAgent()
+
+validation_result = validate(agent_x=my_agent, agent_o=validation_agent, iterations=100)
+ 
+plot_validation(validation_result)
 
 #train functie aanroepen
 train(my_agent, 3000)
@@ -26,6 +43,3 @@ save(my_agent, 'MyAgent_3000')
 start(player_x=my_agent)
 
 
-#agent worst opnieuw aageroepen
-my_agent = load('MyAgent_3000')
-my_agent.learning = False
