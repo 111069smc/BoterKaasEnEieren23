e2cd22f169b56b2ea13bbfc332d7d035f2ed3279
110953smc <95483934+110953smc@users.noreply.github.com>
2023-09-29 09:24:50 +0000

    opnieuw begonnen -> basic spel + de agent trainen

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import random
- 
-from bke import MLAgent, is_winner, opponent, RandomAgent, train_and_plot, load, save
- 
- 
+from bke import MLAgent, is_winner, opponent, load, train, save, start
+
+
 class MyAgent(MLAgent):
     def evaluate(self, board):
         if is_winner(board, self.symbol):
@@ -12,36 +10,22 @@ class MyAgent(MLAgent):
         else:
             reward = 0
         return reward
+    
+
+my_agent = MyAgent()
+
+#agent worst opnieuw aageroepen
+my_agent = load('MyAgent_3000')
+my_agent.learning = False
+
+#train functie aanroepen
+train(my_agent, 3000)
+
+save(my_agent, 'MyAgent_3000')
+
+start(player_x=my_agent)
 
 
-
-for a in range(1, 5):
-    for e in range(1, 11):
-        random.seed(1)
-        print("a is ", (a/100))
-        print("e is ", (e/100))
-        my_agent = MyAgent(alpha=(1/10), epsilon=(10/10))
-        #my_agent = load('MyAgent_3000')
-        #my_agent.learning = False
-
-        random_agent = RandomAgent()
-        train_and_plot(
-            agent=my_agent,
-            validation_agent=random_agent,
-            iterations=85,
-            trainings=100,
-            validations=2500)
-
-random.seed(1)
-
-my_agent = MyAgent(alpha=(0.03), epsilon=(0.5))
-#my_agent = load('MyAgent_3000')
-#my_agent.learning = False
-
-random_agent = RandomAgent()
-train_and_plot(
-    agent=my_agent,
-    validation_agent=random_agent,
-    iterations=50,
-    trainings=100,
-    validations=1000)
+#agent worst opnieuw aageroepen
+my_agent = load('MyAgent_3000')
+my_agent.learning = False
